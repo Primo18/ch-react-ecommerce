988e2a2eb8d1da84597c06fcbd5e76c12299c875
Primo18 <danilobo.elo@gmail.com>
2023-07-18 22:26:50 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Counter from './components/Counter'
+import User from './components/User.jsx'
+import ToggleText from './components/ToggleText.jsx'
+
+function App() {
+  return (
+    <>
+      <User name="Daniel" age={30} profession="Developer" />
+      <Counter />
+      <ToggleText initial="Hello" alternate="World" />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/User.jsx
@@ -0,0 +1,15 @@
+const User = ({ name, age, profession }) => {
+    return (
+        <>
+            <h1>Name: {name}</h1>
+            <h2>Age: {age}</h2>
+            <h2>Profession: {profession}</h2>
+        </>
+    )
+}
+
+export default User
+
+
+
+
