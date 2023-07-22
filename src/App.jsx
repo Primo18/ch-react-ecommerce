84dbe76e368f34aafc25970d0fa2dbe78a382bbf
Primo18 <danilobo.elo@gmail.com>
2023-07-21 23:37:58 -0400

    agregar scroll to top component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css';
+import ScrollToTop from './components/ScrollToTop';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
-// Este componente muestra la aplicación principal
 function App() {
-  // Aquí debes obtener el estado del carrito desde un contexto o un estado global
+  // Aquí se debe obtener el estado del carrito desde un contexto o un estado global
   const cart = [
     { id: 1, name: 'Producto A', price: 1000, quantity: 2 },
     { id: 2, name: 'Producto B', price: 2000, quantity: 3 },
@@ -19,6 +18,7 @@ function App() {
     <>
       <BrowserRouter>
         <NavBar cart={cart} />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<ItemListContainer greeting="Bienvenido a Feria Libre Digital" />} />
           <Route path="/categoria/:categoryId" element={<ItemListContainer greeting="Bienvenido a Feria Libre Digital" />} />
