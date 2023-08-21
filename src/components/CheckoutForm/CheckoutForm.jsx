b60ed23577071be0466acfef628f963f0114ff50
Primo18 <danilobo.elo@gmail.com>
2023-08-20 23:55:19 -0400

    add form validation

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -5,15 +5,15 @@ const CheckoutForm = ({ handleSubmit }) => {
             <form className="form-contact" onSubmit={handleSubmit}>
                 <label htmlFor="nombre" className=''>
                     <span>Nombre</span>
-                    <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" />
+                    <input type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" required />
                 </label>
                 <label htmlFor="email">
                     <span>Email</span>
-                    <input type="email" id="email" name="email" placeholder="Ingrese su email" />
+                    <input type="email" id="email" name="email" placeholder="Ingrese su email" required />
                 </label>
                 <label htmlFor="phone">
                     <span>Telefono</span>
-                    <input type="tel" id="phone" name="phone" placeholder="Ingrese su telefono" />
+                    <input type="tel" id="phone" name="phone" placeholder="Ingrese su telefono" required />
                 </label>
                 <button type="submit">Enviar</button>
             </form>
