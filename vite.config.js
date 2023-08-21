418a7e03e5990c4863bfd753399409facea0d511
Primo18 <danilobo.elo@gmail.com>
2023-08-21 00:25:37 -0400

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://react.ferialibre.app/",
-  plugins: [react()],
-})
